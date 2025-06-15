7f2ae791243d476ddc42fe5b01e4f05f5dcbf55d
Mark Skelton <markskelton10@gmail.com>
2025-06-15 13:54:07 +0100

    test: create #place_token tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -77,4 +77,222 @@ RSpec.describe Board do
       end
     end
   end
+
+  describe '#place_token' do
+    # Incoming Command Message -> Check change of observable state
+
+    context 'when the grid is empty' do
+      subject(:board_place_empty) { described_class.new }
+      it 'updates the grid with a blue token at the bottom of column 0, as intended' do
+        expected_grid =
+          [
+            %w[⚫ ⚫ ⚫ ⚫ ⚫ ⚫ ⚫],
+            %w[⚫ ⚫ ⚫ ⚫ ⚫ ⚫ ⚫],
+            %w[⚫ ⚫ ⚫ ⚫ ⚫ ⚫ ⚫],
+            %w[⚫ ⚫ ⚫ ⚫ ⚫ ⚫ ⚫],
+            %w[⚫ ⚫ ⚫ ⚫ ⚫ ⚫ ⚫],
+            %w[🔵 ⚫ ⚫ ⚫ ⚫ ⚫ ⚫]
+          ]
+        board_place_empty.place_token('🔵', 0)
+        actual_grid = board_place_empty.grid
+        expect(actual_grid).to eql(expected_grid)
+      end
+    end
+
+    context 'when the grid is normal: non-empty and non-full with a mixture of colored disks' do
+      let(:normal_grid) do
+        [
+          %w[🔵 ⚫ ⚫ ⚫ ⚫ ⚫ ⚫],
+          %w[⚫ ⚫ ⚫ ⚫ ⚫ ⚫ ⚫],
+          %w[⚫ ⚫ ⚫ ⚫ 🔴 ⚫ ⚫],
+          %w[⚫ ⚫ ⚫ ⚫ ⚫ ⚫ ⚫],
+          %w[⚫ ⚫ 🔴 🔵 🔴 ⚫ ⚫],
+          %w[⚫ ⚫ 🔴 🔴 🔵 ⚫ ⚫]
+        ]
+      end
+      subject(:board_place_col1_normal) { described_class.new(normal_grid) }
+      it 'updates the grid with a red token at the bottom of column 1, as intended' do
+        expected_grid =
+          [
+            %w[🔵 ⚫ ⚫ ⚫ ⚫ ⚫ ⚫],
+            %w[⚫ ⚫ ⚫ ⚫ ⚫ ⚫ ⚫],
+            %w[⚫ ⚫ ⚫ ⚫ 🔴 ⚫ ⚫],
+            %w[⚫ ⚫ ⚫ ⚫ ⚫ ⚫ ⚫],
+            %w[⚫ ⚫ 🔴 🔵 🔴 ⚫ ⚫],
+            %w[⚫ 🔴 🔴 🔴 🔵 ⚫ ⚫]
+          ]
+        board_place_col1_normal.place_token('🔴', 1)
+        actual_grid = board_place_col1_normal.grid
+        expect(actual_grid).to eql(expected_grid)
+      end
+    end
+
+    context 'when the grid is normal' do
+      let(:normal_grid) do
+        [
+          %w[🔵 ⚫ ⚫ ⚫ ⚫ ⚫ ⚫],
+          %w[⚫ ⚫ ⚫ ⚫ ⚫ ⚫ ⚫],
+          %w[⚫ ⚫ ⚫ ⚫ 🔴 ⚫ ⚫],
+          %w[⚫ ⚫ ⚫ ⚫ ⚫ ⚫ ⚫],
+          %w[⚫ ⚫ 🔴 🔵 🔴 ⚫ ⚫],
+          %w[⚫ ⚫ 🔴 🔴 🔵 ⚫ ⚫]
+        ]
+      end
+      subject(:board_place_col2_normal) { described_class.new(normal_grid) }
+      it 'updates the grid with a blue token on top of the red tokens in column 2, as intended' do
+        expected_grid =
+          [
+            %w[🔵 ⚫ ⚫ ⚫ ⚫ ⚫ ⚫],
+            %w[⚫ ⚫ ⚫ ⚫ ⚫ ⚫ ⚫],
+            %w[⚫ ⚫ ⚫ ⚫ 🔴 ⚫ ⚫],
+            %w[⚫ ⚫ 🔵 ⚫ ⚫ ⚫ ⚫],
+            %w[⚫ ⚫ 🔴 🔵 🔴 ⚫ ⚫],
+            %w[⚫ ⚫ 🔴 🔴 🔵 ⚫ ⚫]
+          ]
+        board_place_col2_normal.place_token('🔵', 2)
+        actual_grid = board_place_col2_normal.grid
+        expect(actual_grid).to eql(expected_grid)
+      end
+    end
+
+    context 'when the grid is normal' do
+      let(:normal_grid) do
+        [
+          %w[🔵 ⚫ ⚫ ⚫ ⚫ ⚫ ⚫],
+          %w[⚫ ⚫ ⚫ ⚫ ⚫ ⚫ ⚫],
+          %w[⚫ ⚫ ⚫ ⚫ 🔴 ⚫ ⚫],
+          %w[⚫ ⚫ ⚫ ⚫ ⚫ ⚫ ⚫],
+          %w[⚫ ⚫ 🔴 🔵 🔴 ⚫ ⚫],
+          %w[⚫ ⚫ 🔴 🔴 🔵 ⚫ ⚫]
+        ]
+      end
+      subject(:board_place_col4_normal) { described_class.new(normal_grid) }
+      it 'updates the grid with a blue token on top of the other tokens in column 4, as intended' do
+        expected_grid =
+          [
+            %w[🔵 ⚫ ⚫ ⚫ ⚫ ⚫ ⚫],
+            %w[⚫ ⚫ ⚫ ⚫ 🔵 ⚫ ⚫],
+            %w[⚫ ⚫ ⚫ ⚫ 🔴 ⚫ ⚫],
+            %w[⚫ ⚫ ⚫ ⚫ ⚫ ⚫ ⚫],
+            %w[⚫ ⚫ 🔴 🔵 🔴 ⚫ ⚫],
+            %w[⚫ ⚫ 🔴 🔴 🔵 ⚫ ⚫]
+          ]
+        board_place_col4_normal.place_token('🔵', 4)
+        actual_grid = board_place_col4_normal.grid
+        expect(actual_grid).to eql(expected_grid)
+      end
+    end
+
+    context 'when the grid is normal' do
+      let(:normal_grid) do
+        [
+          %w[🔵 ⚫ ⚫ ⚫ ⚫ ⚫ ⚫],
+          %w[⚫ ⚫ ⚫ ⚫ ⚫ ⚫ ⚫],
+          %w[⚫ ⚫ ⚫ ⚫ 🔴 ⚫ ⚫],
+          %w[⚫ ⚫ ⚫ ⚫ ⚫ ⚫ ⚫],
+          %w[⚫ ⚫ 🔴 🔵 🔴 ⚫ ⚫],
+          %w[⚫ ⚫ 🔴 🔴 🔵 ⚫ ⚫]
+        ]
+      end
+      subject(:board_place_col0_normal) { described_class.new(normal_grid) }
+      it 'does not update the grid when the token is blocked from a column by an existing token, as intended' do
+        expected_grid =
+          [
+            %w[🔵 ⚫ ⚫ ⚫ ⚫ ⚫ ⚫],
+            %w[⚫ ⚫ ⚫ ⚫ ⚫ ⚫ ⚫],
+            %w[⚫ ⚫ ⚫ ⚫ 🔴 ⚫ ⚫],
+            %w[⚫ ⚫ ⚫ ⚫ ⚫ ⚫ ⚫],
+            %w[⚫ ⚫ 🔴 🔵 🔴 ⚫ ⚫],
+            %w[⚫ ⚫ 🔴 🔴 🔵 ⚫ ⚫]
+          ]
+        board_place_col0_normal.place_token('🔴', 0)
+        actual_grid = board_place_col0_normal.grid
+        expect(actual_grid).to eql(expected_grid)
+      end
+    end
+
+    context 'when the grid is normal' do
+      let(:normal_grid) do
+        [
+          %w[🔵 ⚫ ⚫ ⚫ ⚫ ⚫ ⚫],
+          %w[⚫ ⚫ ⚫ ⚫ ⚫ ⚫ ⚫],
+          %w[⚫ ⚫ ⚫ ⚫ 🔴 ⚫ ⚫],
+          %w[⚫ ⚫ ⚫ ⚫ ⚫ ⚫ ⚫],
+          %w[⚫ ⚫ 🔴 🔵 🔴 ⚫ ⚫],
+          %w[⚫ ⚫ 🔴 🔴 🔵 ⚫ ⚫]
+        ]
+      end
+      subject(:board_place_negative_normal) { described_class.new(normal_grid) }
+      it 'does not update the grid when the column is negative' do
+        expected_grid =
+          [
+            %w[🔵 ⚫ ⚫ ⚫ ⚫ ⚫ ⚫],
+            %w[⚫ ⚫ ⚫ ⚫ ⚫ ⚫ ⚫],
+            %w[⚫ ⚫ ⚫ ⚫ 🔴 ⚫ ⚫],
+            %w[⚫ ⚫ ⚫ ⚫ ⚫ ⚫ ⚫],
+            %w[⚫ ⚫ 🔴 🔵 🔴 ⚫ ⚫],
+            %w[⚫ ⚫ 🔴 🔴 🔵 ⚫ ⚫]
+          ]
+        board_place_negative_normal.place_token('🔴', -1)
+        actual_grid = board_place_negative_normal.grid
+        expect(actual_grid).to eql(expected_grid)
+      end
+    end
+
+    context 'when the grid is normal' do
+      let(:normal_grid) do
+        [
+          %w[🔵 ⚫ ⚫ ⚫ ⚫ ⚫ ⚫],
+          %w[⚫ ⚫ ⚫ ⚫ ⚫ ⚫ ⚫],
+          %w[⚫ ⚫ ⚫ ⚫ 🔴 ⚫ ⚫],
+          %w[⚫ ⚫ ⚫ ⚫ ⚫ ⚫ ⚫],
+          %w[⚫ ⚫ 🔴 🔵 🔴 ⚫ ⚫],
+          %w[⚫ ⚫ 🔴 🔴 🔵 ⚫ ⚫]
+        ]
+      end
+      subject(:board_place_too_large_normal) { described_class.new(normal_grid) }
+      it 'does not update the grid when the column is too large: 7' do
+        expected_grid =
+          [
+            %w[🔵 ⚫ ⚫ ⚫ ⚫ ⚫ ⚫],
+            %w[⚫ ⚫ ⚫ ⚫ ⚫ ⚫ ⚫],
+            %w[⚫ ⚫ ⚫ ⚫ 🔴 ⚫ ⚫],
+            %w[⚫ ⚫ ⚫ ⚫ ⚫ ⚫ ⚫],
+            %w[⚫ ⚫ 🔴 🔵 🔴 ⚫ ⚫],
+            %w[⚫ ⚫ 🔴 🔴 🔵 ⚫ ⚫]
+          ]
+        board_place_too_large_normal.place_token('🔵', 7)
+        actual_grid = board_place_too_large_normal.grid
+        expect(actual_grid).to eql(expected_grid)
+      end
+    end
+
+    context 'when the grid is full of tokens of different colors' do
+      let(:full_grid) do
+        [
+          %w[🔵 🔵 🔴 🔵 🔵 🔵 🔴],
+          %w[🔴 🔴 🔴 🔴 🔵 🔴 🔵],
+          %w[🔴 🔵 🔴 🔵 🔴 🔵 🔴],
+          %w[🔵 🔵 🔴 🔴 🔵 🔵 🔵],
+          %w[🔵 🔴 🔴 🔵 🔴 🔵 🔴],
+          %w[🔵 🔵 🔴 🔴 🔵 🔴 🔵]
+        ]
+      end
+      subject(:board_place_full) { described_class.new(full_grid) }
+      it 'does not update the grid' do
+        expected_grid =
+          [
+            %w[🔵 🔵 🔴 🔵 🔵 🔵 🔴],
+            %w[🔴 🔴 🔴 🔴 🔵 🔴 🔵],
+            %w[🔴 🔵 🔴 🔵 🔴 🔵 🔴],
+            %w[🔵 🔵 🔴 🔴 🔵 🔵 🔵],
+            %w[🔵 🔴 🔴 🔵 🔴 🔵 🔴],
+            %w[🔵 🔵 🔴 🔴 🔵 🔴 🔵]
+          ]
+        board_place_full.place_token('🔴', 6)
+        actual_grid = board_place_full.grid
+        expect(actual_grid).to eql(expected_grid)
+      end
+    end
+  end
 end
