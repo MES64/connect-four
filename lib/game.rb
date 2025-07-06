46c30620b40fd91703a0472b41a636a8aa96d89d
Mark Skelton <markskelton10@gmail.com>
2025-07-06 17:19:14 +0100

    feat: create #result method of Game

---
+++ lib/game.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# Game objects represent the state of the connect four game
+class Game
+  attr_reader :board
+
+  def initialize(board:)
+    @board = board
+  end
+
+  def result
+    red_four = board.four_in_row?('🔴')
+    blue_four = board.four_in_row?('🔵')
+    return 'Unknown' if red_four && blue_four
+    return "#{red_four ? '🔴' : '🔵'} Wins!" if red_four || blue_four
+
+    'Draw!' if board.full?
+  end
+end
