865a28fdb75cbed8c2e9a87e691488844d940e39
Mark Skelton <markskelton10@gmail.com>
2025-07-06 19:20:41 +0100

    test: create #make_move tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -116,4 +116,54 @@ RSpec.describe Game do
       end
     end
   end
+
+  describe '#make_move' do
+    # Looping Script -> Test behavior
+    # The user inputs until the move is valid with puts and gets, then places the token
+    # This gives it multiple responsibility, as it sends command messages to both standard
+    # output and the Board object that are well tested, and hence is a type of script method
+    # that loops.
+
+    let(:board) { instance_double(Board) }
+    subject(:game_move) { described_class.new(board:) }
+
+    before do
+      allow(board).to receive(:place_token)
+      allow(game_move).to receive(:puts)
+      allow(game_move).to receive(:gets)
+    end
+
+    context 'when the input is valid' do
+      before do
+        allow(board).to receive(:valid_move?).and_return(true)
+      end
+
+      it 'does not show the invalid input message' do
+        expect(game_move).to_not receive(:puts).with('Invalid Input!')
+        game_move.make_move
+      end
+    end
+
+    context 'when the input is invalid, then valid' do
+      before do
+        allow(board).to receive(:valid_move?).and_return(false, true)
+      end
+
+      it 'shows the invalid input message exactly once' do
+        expect(game_move).to receive(:puts).with('Invalid Input!').exactly(1).time
+        game_move.make_move
+      end
+    end
+
+    context 'when the input is invalid four times in a row, then valid' do
+      before do
+        allow(board).to receive(:valid_move?).and_return(false, false, false, false, true)
+      end
+
+      it 'shows the invalid input message exactly four times' do
+        expect(game_move).to receive(:puts).with('Invalid Input!').exactly(4).times
+        game_move.make_move
+      end
+    end
+  end
 end
